06187f92f85ee10a1683a33d1fe94d584fc58432
Srinivas Saravanan A <srinivassaravanan@venbainfotech.com>
2025-05-01 22:22:23 +0530

    feat_routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,10 +2,36 @@
 
  const router = express.Router();
 
+
+ //get
  router.get("/sayHello",(req,res) => {
 
     res.json({message :"Hello From Srinivas"})
 
  })
 
+ router.get("/sayHello/:id",(req,res) => {
+   res.json({get : "Single Workout"})
+ })
+
+
+ router.post("/sayHello",(req,res) => {
+   res.json({msg:"Hello from Srinivas in Post"})
+ })
+
+ router.delete("/sayHello/:id",(req,res) => {
+   res.json({msg:"Hello from Srinivas in Delete"})
+ })
+
+ router.patch("/sayHello/:id",(req,res) => {
+   res.json({msg:"Hello from Srinivas in Patch"})
+ })
+
+
+
+ router.delete("/delete/:id",(req,res) => {
+   res.json({msg:"Hello from Srinivas in Patch"})
+ })
+
+ 
  module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 require("dotenv").config();
 
 const express = require("express");
-const workoutRoutes = require('/routes/workouts')
+const workoutRoutes = require('./routes/workouts')
 
 
 // Exp app
 const app = express();
 
 // app middleware
+
+app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next();
 });
 
-//App Routes
 
+//App Routes
 app.use("/srinivas/",workoutRoutes)
 
 

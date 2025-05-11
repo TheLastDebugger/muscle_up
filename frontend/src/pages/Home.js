99b85b8e392cfe8d3f796998e3f8ed78683ea277
Srinivas Saravanan A <srinivassaravanan2002@gmail.com>
2025-05-11 09:33:30 +0530

    USEEFFECT

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,24 @@ import { useEffect,useState } from "react"
 
 const Home = () =>{
 
+    const [workouts,setWorkouts] = useState(null)
+
     useEffect(() => {
+
+        const fetchWorkouts = async () => {
+             
+            const respone = await fetch('http://localhost:4000/api/workouts')
+
+            const json = await Response.json()
+
+            if (reponse.ok) {
+
+            }
+        }
+
+        fetchWorkouts()
         
-    });
+    },[]);
 
     return (
         <div className="home">
